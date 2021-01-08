23b89392bd44054cf66350ed52928d82c10e7a12
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2021-01-08 18:02:50 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,12 +17,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
         .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.5"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .revision("HEAD")),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
     ],
     targets: [
         .target(
             name: "QMobileAPI",
-            dependencies: ["XCGLogger", "Moya", "Alamofire"],
+            dependencies: ["XCGLogger", "Moya", "Alamofire", "SwiftyJSON"],
             path: "Sources")
     ]
 )
